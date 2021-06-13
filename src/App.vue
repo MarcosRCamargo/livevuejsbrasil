3e4d4a8af6901146f1718797330e27d89e113fd8
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-06-13 20:31:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <h1>Minha Lista</h1>
+  <input 
+    id="addfoodselement"
+    @keyup.enter="addFood"
+    type="text" v-model="currentFood">
+  <button @click="addFood">Add</button>
+  <food-list
+  @My-event="handleClick" 
+  :foods="foods"
+  />
+</div>
+
+</template>
+
+<script>
+// import FoodList from './components/FoodList'
+import FoodList from './components/FoodList.vue';
+export default {
+  components: {FoodList},
+  data: () => ({
+  name: "Marcos Camargo",
+  color: "tomato",
+  foods: ['Maça', 'Pera'],
+  }),
+  methods:{
+    handleClick(){
+      window.alert('Clicado!')
+    },
+    addFood(){
+      window.alert(this.currentFood)
+      if(this.currentFood === undefined && this.currentFood === null && this.currentFood === ''){
+        window.alert('É informe uma comida')
+        this.$refs.addfoodselement.focus()
+      }
+      this.foods.push(this.currentFood)
+      this.currentFood = ''
+    }
+  }
+};
+</script>
---
+++ src/components/FoodList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="(food, index) in foods" 
+    :key="`${food}-${index}`">
+      {{ food }}
+    </li>
+  </ul>
+</template>
+<script>
+
+const MAX_FOOD  = 5;
+
+export default ({
+    props: {
+        foods: { type: Array, default: () => [] }
+    },
+    watch:{
+      foods (){
+        if(this.foods.length >= MAX_FOOD){
+          this.$emit('stop', 'Marcos Falou que ta cheio');""
+        }
+        console.log('chenged', this.foods.length)
+      }
+    }
+})
+</script>
